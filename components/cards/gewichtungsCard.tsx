f74cef80d0da9d939d2810a0079a6f7d2e03c9f1
eycan <128330344+eycan@users.noreply.github.com>
2026-05-18 00:20:02 +0200

    First try to fix stuck hover on mobile

--- components/cards/gewichtungsCard.tsx
+++ components/cards/gewichtungsCard.tsx
@@ -54,12 +54,12 @@ const GewichtungsCard = forwardRef<HTMLDivElement, GewichtungsCardProps>(
       onClick={onClick}
       ref={setBarRef}
       tabIndex={0}
-      className="relative bg-surface p-4 rounded-lg hover:ring-2 hover:ring-highlight overflow-hidden focus:scale-105 cursor-pointer"
+      className="relative bg-surface p-4 rounded-lg md:hover:ring-2 md:hover:ring-highlight overflow-hidden md:focus:scale-105 cursor-pointer"
     >
       <p
         onClick={mark}
         ref={markerRef}
-        className="absolute inset-0 flex justify-start items-center opacity-0 hover:opacity-100 ml-5 font-bold text-highlight text-lg md:text-xl transition-opacity duration-300"
+        className="absolute inset-0 flex justify-start items-center opacity-0 md:hover:opacity-100 ml-5 font-bold text-highlight text-lg md:text-xl transition-opacity duration-300"
       >
         2x
       </p>
